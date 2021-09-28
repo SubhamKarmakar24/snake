7cd92ccbdadd0bbb9f4d2c851106075d2ae9cb9c
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-09-28 11:58:35 +0530

    Prettier

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
- function choose_difficulty(maze_type){
-    if(maze_type == null){
+function choose_difficulty(maze_type)
+{
+    if(maze_type == null)
+    {
         window.location.href = "index.html"
     }
-    else{
+    else
+    {
         window.location.href = "difficulty.html?maze=" + maze_type
     }
 }
 
-function choose_challenge_type(challenge,increment, speed){
-    if(increment == null || speed == null || challenge ==  null){
+function choose_challenge_type(challenge,increment, speed)
+{
+    if(increment == null || speed == null || challenge ==  null)
+    {
         window.location.href = "index.html"
     }
-    else{
+    else
+    {
         window.location.href = challenge + ".html?increment=" +  increment + "&speed=" +  speed
     }
 }
